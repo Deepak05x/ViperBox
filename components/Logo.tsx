6f04fd115bb38ecb60c4f5ff72cf92f19781763f
Deepak <hdeepak5902@gmail.com>
2024-09-11 00:01:38 +0530

    Logo done

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='text-2xl font-bold'>
+        <p>Viper<span className='text-green-500'>box</span></p>
+    </div>
+  )
+}
+
+export default Logo
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import dynamic from 'next/dynamic'
+
+const Logo = dynamic(()=>import('@/components/Logo'))
 
 const Navbar = () => {
   return (
     <div>
-      
+        <Logo />
     </div>
   )
 }
