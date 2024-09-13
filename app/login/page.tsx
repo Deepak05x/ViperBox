8458fcfc13534e1bbda5859bbb6fca709fb31a23
Deepak <hdeepak5902@gmail.com>
2024-09-14 00:09:30 +0530

    shadcn

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Input } from '@/components/ui/input'
 
 const Login = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='flex '>
+        <h1>Already have an account</h1>
+        <form action="">
+            <div>
+                <Input />
+            </div>
+        </form>
+    </section>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic'
 import {FaArrowRight} from 'react-icons/fa'
 import { PiShootingStarFill } from "react-icons/pi";
 import Link from 'next/link';
+import { Button } from './ui/button';
 
 const Logo = dynamic(()=>import('@/components/Logo'))
 
@@ -11,15 +12,17 @@ const Navbar = () => {
     <nav className='flex fixed z-30 w-full items-center justify-between py-6 px-12 bg-white'>
         <Logo />
         <section className='flex items-center gap-12 text-lg font-medium'>
-          <Link href={'/login'} className='cursor-pointer '>Login</Link>
+          <Button variant={'destructive'}>
+              <Link href={'/login'} className='cursor-pointer '>Login</Link>
+          </Button>
           <p className='cursor-pointer flex items-center gap-1'>
             Dashboard
             <PiShootingStarFill className='text-yellow-500' />
           </p>
-          <button className='bg-green text-white flex items-center justify-center gap-2 py-1 px-2 rounded-lg cursor-pointer'>
+          <Button className='flex items-center gap-1 justify-center'>
             <p>Create Case</p>
             <FaArrowRight className='text-sm' />
-          </button>
+          </Button>
         </section>
     </nav>
   )
