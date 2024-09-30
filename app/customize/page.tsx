b9d362cb09970123147cb1f2d1aee3b5b050c981
Deepak <hdeepak5902@gmail.com>
2024-09-30 20:24:56 +0530

    made the border responsive

--- app/customize/page.tsx
+++ app/customize/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import dynamic from "next/dynamic";
+
+const Intro = dynamic(() => import("@/components/Create/Intro"));
+const Navbar = dynamic(() => import("@/components/Navbar"));
 
 const Customize = () => {
-    return <div>Customize</div>;
+    return (
+        <main>
+            <Navbar />
+            <section>
+                <Intro borderColor={"border-green"} />
+            </section>
+        </main>
+    );
 };
 
 export default Customize;
--- app/summary/page.tsx
+++ app/summary/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import dynamic from "next/dynamic";
+
+const Intro = dynamic(() => import("@/components/Create/Intro"));
+const Navbar = dynamic(() => import("@/components/Navbar"));
 
 const Summary = () => {
-    return <div>Summary</div>;
+    return (
+        <main>
+            <Navbar />
+            <section>
+                <Intro borderColor={"border-green"} />
+            </section>
+        </main>
+    );
 };
 
 export default Summary;
