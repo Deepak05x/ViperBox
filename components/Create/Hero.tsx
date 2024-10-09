568e1cb9f57a8718f2733842c5b385250830f588
Deepak <hdeepak5902@gmail.com>
2024-10-09 19:04:09 +0530

    made the color state

--- components/Create/Hero.tsx
+++ components/Create/Hero.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import { useContext } from "react";
 import { CreateContext } from "@/context/CreateProvider";
+import dynamic from "next/dynamic";
+import { CreateContextType } from "@/context/CreateProvider";
+import { Button } from "../ui/button";
+
+const Phone = dynamic(() => import("../Phone"));
 
 const colors = [
     {
@@ -14,26 +18,38 @@ const colors = [
         name: "Red",
         color: "bg-red-500",
     },
+    {
+        name: "Blue",
+        color: "bg-blue-500",
+    },
 ];
 
-const Hero = () => {
-    const { color, setColor } = useContext(CreateContext);
+const Hero: React.FC = () => {
+    const { colorName, setColorName, colorType, setColorType } = useContext<CreateContextType>(CreateContext);
+
+    const handleColorClick = (name: string, type: string): void => {
+        setColorName(name);
+        setColorType(type);
+    };
 
     return (
-        <section className="flex flex-row w-full items-center  justify-center h-[70vh]">
-            <section className="bg-gray-100 h-full flex items-center justify-center px-[13rem] py-24">
-                <Image src={"/phone-template.png"} alt="phone" width={200} height={200} />
+        <section className="flex flex-row w-full items-center  justify-center h-[80vh] relative">
+            <section className="bg-gray-100 h-full flex flex-col gap-12 items-center justify-center px-[13rem] py-8 border-2 border-gray-300 rounded-xl">
+                <Button variant={"default"} className="order-2">
+                    Upload
+                </Button>
+                <Phone imgSrc="" className={`w-60 ${colorType} order-1`} />
             </section>
-            <section className="flex flex-col overflow-y-auto h-full px-24 gap-8 ">
-                <h1 className="text-lg font-medium">Customize your case</h1>
+            <section className="flex flex-col overflow-y-auto bg-gray-50 h-full px-16 gap-8 ">
+                <h1 className="text-lg font-bold">Customize your case</h1>
                 <hr />
                 <div className="flex flex-col gap-4">
-                    <p>Color: {color}</p>
+                    <p>Color: {colorName}</p>
                     <div className="flex flex-row items-center gap-4">
                         {colors.map((item, index) => (
                             <div
                                 key={index}
-                                onClick={() => setColor(item.name)}
+                                onClick={() => handleColorClick(item.name, item.color)}
                                 className={`${item.color} w-[1.5rem] h-[1.5rem] rounded-full cursor-pointer hover:scale-105 transition ease-in-out`}
                             ></div>
                         ))}
--- context/CreateProvider.tsx
+++ context/CreateProvider.tsx
@@ -3,22 +3,27 @@
 import React, { ReactNode } from "react";
 import { useState, createContext } from "react";
 
-interface CreateContextType {
-    color: string;
-    setColor: React.Dispatch<React.SetStateAction<string>>;
+export interface CreateContextType {
+    colorName: string;
+    colorType: string;
+    setColorName: React.Dispatch<React.SetStateAction<string>>;
+    setColorType: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const CreateContextIntital = {
-    color: "",
-    setColor: () => {},
+const CreateContextIntital: CreateContextType = {
+    colorName: "",
+    colorType: "",
+    setColorName: () => {},
+    setColorType: () => {},
 };
 
 export const CreateContext = createContext<CreateContextType>(CreateContextIntital);
 
 const CreateProvider = ({ children }: { children: ReactNode }) => {
-    const [color, setColor] = useState<string>("black");
+    const [colorName, setColorName] = useState<string>("Black");
+    const [colorType, setColorType] = useState<string>("bg-black");
 
-    return <CreateContext.Provider value={{ color, setColor }}>{children}</CreateContext.Provider>;
+    return <CreateContext.Provider value={{ colorName, setColorName, colorType, setColorType }}>{children}</CreateContext.Provider>;
 };
 
 export default CreateProvider;
